03e0f89036907e0f4ec69c3931f86b237b238dbd
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-18 22:24:56 +0700

    404 error
    404 error

--- application/config/routes.php
+++ application/config/routes.php
@@ -39,7 +39,7 @@
 */
 
 $route['default_controller'] = "welcome";
-$route['404_override'] = 'views/welcome_message';
+$route['404_override'] = 'views/welcome_message.php';
 
 
 /* End of file routes.php */
