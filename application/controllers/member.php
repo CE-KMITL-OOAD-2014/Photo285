206266440333e2bd285e043bd5cd734098e4c0fa
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-19 03:53:26 +0700

    add database
    add database

---
+++ application/controllers/member.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class member extends CI_Controller {
+	public function register(){
+			$username = $_POST["username"];
+			$password = md5($_POST["password"]);
+			$namejing = $_POST["namejing"];
+			$data = array('username'=>$username,'password'=>$password,'namejing'=>$namejing);
+			$this->load->model('member_model');	
+			$this->member_model->register($data);
+		}
+}
+?>
---
+++ application/views/register.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
+<form action="../member/register" method="post" align="center"> 
+<h3>Register</h3><br/>
+<p>id : <input type="text" name="username"/></p>
+<p>password : <input type="password" name="password"/></p>
+<p>name : <input type="text" name="namejing"/></p>
+<input type="submit" value="OK"/>
+</form>
+</body>
+</html>
