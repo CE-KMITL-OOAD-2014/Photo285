7303a921d0c1ba5ff2556dd74dc08d069883e0e4
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-19 03:49:28 +0700

    test
    test

--- application/models/member_model.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-	class Member_model extends CI_Model{
-		function register($data){
-			$this->db->insert('UserDBE',$data);	
-		}	
-	}
-?>
