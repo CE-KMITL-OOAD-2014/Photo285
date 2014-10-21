ce1cee2d9f83748ecc7c2a7d26924cab07a80cfd
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 05:47:16 +0700

    test

--- application/views/test.php
+++ application/views/test.php
@@ -12,59 +12,37 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
   </head>
-<body>
-<form id="identicalForm" class="form-horizontal">
+
+
+<form id="identicalForm" class="form-horizontal"
+    data-bv-feedbackicons-valid="glyphicon glyphicon-ok"
+    data-bv-feedbackicons-invalid="glyphicon glyphicon-remove"
+    data-bv-feedbackicons-validating="glyphicon glyphicon-refresh">
+
     <div class="form-group">
         <label class="col-sm-3 control-label">Password</label>
         <div class="col-sm-5">
-            <input type="password" class="form-control" name="password" />
+            <input type="password" class="form-control" name="password"
+                data-bv-identical="true"
+                data-bv-identical-field="confirmPassword"
+                data-bv-identical-message="The password and its confirm are not the same" />
         </div>
     </div>
 
     <div class="form-group">
         <label class="col-sm-3 control-label">Retype password</label>
         <div class="col-sm-5">
-            <input type="password" class="form-control" name="confirmPassword" />
+            <input type="password" class="form-control" name="confirmPassword"
+                data-bv-identical="true"
+                data-bv-identical-field="password"
+                data-bv-identical-message="The password and its confirm are not the same" />
         </div>
     </div>
-	<script>
-	
-$(document).ready(function() {
-    $('#identicalForm').bootstrapValidator({
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            password: {
-                validators: {
-                    identical: {
-                        field: 'confirmPassword',
-                        message: 'The password and its confirm are not the same'
-                    }
-                }
-            },
-            confirmPassword: {
-                validators: {
-                    identical: {
-                        field: 'password',
-                        message: 'The password and its confirm are not the same'
-                    }
-                }
-            }
-        }
-    });
-});
-
-</script>
 </form>
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <link href="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <link href="js/bootstrap.min.js">
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
+
+<script>
+$(document).ready(function() {
+    $('#identicalForm').bootstrapValidator();
+});
+</script>
+
