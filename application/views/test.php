14015f524327ccacf5c9e98574b85667449f5b58
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 05:37:32 +0700

    test comfirm pass

--- application/views/test.php
+++ application/views/test.php
@@ -3,7 +3,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap 101 Template</title>
-
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
@@ -24,15 +28,6 @@
         </div>
     </div>
 	<script>
-	
-$('form').on('submit',function(){
-   if($('#password').val()!=$('#confirmPassword').val()){
-       alert('Password not matches');
-       return false;
-   }
-   return true;
-});
-	
 	
 $(document).ready(function() {
     $('#identicalForm').bootstrapValidator({
@@ -64,5 +59,7 @@ $(document).ready(function() {
 
 </script>
 </form>
+
+
 
 </body>
