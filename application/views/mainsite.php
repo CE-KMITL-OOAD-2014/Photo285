c0f1ccd52524c3dfea6368560048c41c79bbdc5a
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 05:14:31 +0700

    alert password

--- application/views/mainsite.php
+++ application/views/mainsite.php
@@ -143,44 +143,15 @@
 								</div>
 							</div>
 						</div> <!--ส่วนกรอก CONFIRMPASSWAORD---->
-						
-						
-
-<form id="identicalForm" class="form-horizontal"
-    data-bv-feedbackicons-valid="glyphicon glyphicon-ok"
-    data-bv-feedbackicons-invalid="glyphicon glyphicon-remove"
-    data-bv-feedbackicons-validating="glyphicon glyphicon-refresh">
-
-    <div class="form-group">
-        <label class="col-sm-3 control-label">Password</label>
-        <div class="col-sm-5">
-            <input type="password" class="form-control" name="password"
-                data-bv-identical="true"
-                data-bv-identical-field="confirmPassword"
-                data-bv-identical-message="The password and its confirm are not the same" />
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label class="col-sm-3 control-label">Retype password</label>
-        <div class="col-sm-5">
-            <input type="password" class="form-control" name="confirmPassword"
-                data-bv-identical="true"
-                data-bv-identical-field="password"
-                data-bv-identical-message="The password and its confirm are not the same" />
-        </div>
-    </div>
-</form>
-
-<script>
-$(document).ready(function() {
-    $('#identicalForm').bootstrapValidator();
-});
-</script>
-
-
-						
-						
+						<script>
+						$('form').on('submit',function(){
+						   if($('#pass').val()!=$('#cpass').val()){
+							   alert('Password not matches');
+							   return false;
+						   }
+						   return true;
+						});
+						</script>
 						<div class="col-md-12"> <!------ส่วนกด SUMMIT--->
 							<div class="col-md-12">
 							<button type="submit" value="OK" class="btn btn-default">Submit</button>
