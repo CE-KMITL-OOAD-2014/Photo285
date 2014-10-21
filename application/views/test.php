5c6ad5dd0be636a68ab168d35e568c70bc6f01da
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 05:39:46 +0700

    test pass

--- application/views/test.php
+++ application/views/test.php
@@ -60,6 +60,8 @@ $(document).ready(function() {
 </script>
 </form>
 
-
-
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
