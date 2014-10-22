d4598d3a280529e93d656e6a88ec964612f1d28c
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-23 05:26:47 +0700

    login codeignite

--- application/controllers/test2.php
+++ application/controllers/test2.php
@@ -1,9 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
+ 
 class Test2 extends CI_Controller {
-
-	public function index()
+ 
+	function __construct()
 	{
-		$this->load->view('profile.php');
+	parent::__construct();
 	}
+ 
+	function index(){
+	$this->load->helper(array('form'));
+	$this->load->view('profile.php');
+	}
+ 
 }
+ 
+?>
--- application/models/member_model.php
+++ application/models/member_model.php
@@ -3,5 +3,20 @@
 		function register($data){
 			$this->db->insert('account',$data);	
 		}	
+		
+		function login($ID, $pass){
+			$this -> db -> select('ID,pass');
+			$this -> db -> from('account');
+			$this -> db -> where('ID', $ID);
+			$this -> db -> where('pass',$pass);
+			$this -> db -> limit(1);
+			$query = $this -> db -> get();
+			if($query -> num_rows() == 1){
+				 return $query->result();
+			}
+			else{
+			return false;
+			}
+		}
 	}
 ?>
