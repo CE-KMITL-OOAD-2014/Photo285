73d532a318809e21fb52704700f4132719d543dc
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 09:33:33 +0700

    test

--- application/controllers/photodb.php
+++ application/controllers/photodb.php
@@ -5,8 +5,9 @@ class Photodb extends CI_Controller {
 		$this->load->model('photo_model');
 		$ID = $this->photo_model->number();
 		foreach($ID as $row)
-		$ID = $row['ID'];
+		$ID = $row['ID']+1;
 		echo $ID;
+		
 		
 	}
 }
