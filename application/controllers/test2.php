cde65bce9e1111290e08820cb47a96b465d217bf
gamegolfgolf <gamegolfg@hotmail.com>
2014-10-23 04:15:55 +0700

    test view profile

---
+++ application/controllers/test2.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test2 extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('profile.php');
+	}
+}
