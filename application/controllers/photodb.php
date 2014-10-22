4fbb7d3295308934d75cdf97dad98f4cc71435e5
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 09:39:55 +0700

    test

--- application/controllers/photodb.php
+++ application/controllers/photodb.php
@@ -3,11 +3,16 @@
 class Photodb extends CI_Controller {
 	public function addPhoto(){
 		$this->load->model('photo_model');
+		//ส่วนออโต้เลขไอดี
 		$ID = $this->photo_model->number();
 		foreach($ID as $row)
 		$ID = $row['ID']+1;
 		echo $ID;
-		
+		//
+		$nameuser = $_POST["nameuser"];
+		$namealbum = $_POST["namealbum"];
+		$data = array('ID'=>$ID,'nameuser'=>$nameuser,'namealbum'=>$namealbum);	
+		$this->photo_model->addPhoto($data);
 		
 	}
 }
