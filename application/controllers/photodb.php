9a70d0edb75184cf2a38ac979c8f3dcb075e7895
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 09:21:27 +0700

    test

--- application/controllers/photodb.php
+++ application/controllers/photodb.php
@@ -4,7 +4,8 @@ class Photodb extends CI_Controller {
 	public function addPhoto(){
 		$this->load->model('photo_model');
 		$ID = $this->photo_model->number();
-		echo"".$ID['ID'];
+		foreach($ID->result_array() as $row)
+		echo"".$row['ID'];
 		
 		
 	}
