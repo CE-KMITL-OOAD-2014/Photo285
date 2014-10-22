861f5de7e62d127b78364362d7f237254bea7d01
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-22 08:11:28 +0700

    test db photo

--- application/views/test.php
+++ application/views/test.php
@@ -3,8 +3,8 @@
 class Test extends CI_Controller {
 	public function addPhoto(){
 		$ID = 1;
-		$nameuser = admin;
-		$namealbum = albumtest;
+		$nameuser = "admin";
+		$namealbum = "albumtest";
 		$data = array('ID'=>$ID,'nameuser'=>$nameuser,'namealbum'=>$namealbum);
 		$this->load->model('photo_model');	
 		$this->member_model->addPhoto($data);
