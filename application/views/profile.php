c5035b618b5342ff4548a8ca1fa75b205efd65ef
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-26 04:37:02 +0700

    test

--- application/views/profile.php
+++ application/views/profile.php
@@ -2,10 +2,9 @@
 if($this->session->userdata('ID')){
 	$ID = $this->session->all_userdata();
 	echo $ID['ID'];
-	/*
-	$data = $this->db->where('ID'=>$ID)->get('account');
+	$data = $this->db->where('ID'=>$ID['ID'])->get('account');
 	echo $data['name'];
 	echo $data['email'];
-	*/
+
  }
 ?>
