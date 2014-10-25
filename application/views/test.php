af1bbd5f971a1168df9f52aa3e285c71b1e546a2
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-26 03:21:45 +0700

    test

--- application/views/test.php
+++ application/views/test.php
@@ -1,5 +1,5 @@
 <form role="form" action="../test/endses" method="post">  
 <input type="text" name="nameuser" class="form-control" id="exampleInputName1" placeholder="Enter name" required autofocus>
 <input type="email" name="namealbum" class="form-control" id="exampleInputEmail1" placeholder="Enter email" required autofocus>
-<button type="submit" value="OK" class="btn btn-default">Submit</button>
+<button type="submit" value="OK" class="btn btn-default">Submitttt</button>
 </form>
