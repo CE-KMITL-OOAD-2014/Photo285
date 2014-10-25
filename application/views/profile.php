bd674f0e9f18e8c2f8997b8930834b2608358b14
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-26 04:26:38 +0700

    test get data

--- application/views/profile.php
+++ application/views/profile.php
@@ -2,7 +2,7 @@
 if($this->session->userdata('ID')){
 	$ID = $this->session->all_userdata();
 	echo $ID['ID'];
-	$data = $this->db->where('ID'=>$ID)->get('acount');
+	$data = $this->db->where('ID'=>$ID)->get('account');
 	echo $data['name'];
 	echo $data['email'];
  }
