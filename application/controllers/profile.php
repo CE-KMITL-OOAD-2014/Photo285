10304053a5b285439afec473613cc8039c2deaf1
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-29 06:57:39 +0700

    set redirect profile

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -4,7 +4,9 @@ class Profile extends CI_Controller {
 
 	public function show(){ // เช็ค / อันที่ 3 ให้เป็นชื่อ username
 		if ($this->uri->segment(3) === FALSE){ // ถ้าไม่มี / อันที่ 3 ให้เซตค่า 0
-			$id = 0;
+			echo"<script language='javascript'>
+			window.location.href = '../';
+			</script>";
 		}
 		else{
 			$id = $this->uri->segment(3); // ถ้ามี / อันที่ 3 เช่น (web.net/profile/show/admin) user คือ admin ก็เอามาเก็บในตัวแปล id
