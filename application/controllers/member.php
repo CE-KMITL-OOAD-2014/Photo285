6f90c3c019d3db6481e8d0dbc8d70cabdcb03f43
theeramed jitsukon <gamegolfg@hotmail.com>
2014-10-29 06:32:14 +0700

    edit . file

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -9,7 +9,7 @@ class Member extends CI_Controller {
 		$data = array('ID'=>$ID,'pass'=>$pass,'name'=>$name,'email'=>$email);
 		$this->load->model('member_model');	
 		$this->member_model->register($data);
-		$this->load->view('signupdone');
+		$this->load->view('signupdone.html');
 		echo"<script language='javascript'>window.location.href = '../';</script>";
 	}
 	public function login(){
