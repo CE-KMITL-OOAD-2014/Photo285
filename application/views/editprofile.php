b7b2cda770cebdde72492033d72e7c1895ea825f
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-09 05:59:54 +0700

    test

--- application/views/editprofile.php
+++ application/views/editprofile.php
@@ -57,7 +57,7 @@
 										<label for="exampleInputUsername1">Username</label>
 									</div>
 									<div class="col-md-12">
-										<input type="text" name="ID" class="form-control " width = "50%" id="exampleInputUsername1" placeholder="### ตรงนี้ต้องเปลี่ยน เป็น USERNAME" readonly="readonly"required autofocus>
+										<input type="text" name="ID" class="form-control " width = "50%" id="exampleInputUsername1" placeholder="../member/getname" readonly="readonly"required autofocus>
 									</div>
 								</div>
 						</div> <!--ส่วนกรอก username------>
