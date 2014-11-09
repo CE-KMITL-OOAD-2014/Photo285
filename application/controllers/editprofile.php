c331eed52538a138c1142ff88f39ad63d7b91ba5
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-09 09:03:02 +0700

    test

--- application/controllers/editprofile.php
+++ application/controllers/editprofile.php
@@ -10,7 +10,7 @@ class Editprofile extends CI_Controller {
 			$this->load->model('member_model');
 			$name = $this->member_model->getName($ID['ID']);
 			$email = $this->member_model->getEmail($ID['ID']);
-			$data = array('name' => $name , 'id' => $ID['ID'] , 'email' => $email);
+			$data = array('name' => $name , 'id' => $ID['ID']);
 			//$temp = array( 'detail' => $detail, 'pdata' => $pdata);
 			$this->load->view('editprofile',$data);
 		}
