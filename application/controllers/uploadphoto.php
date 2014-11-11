b2dda27a6c9d1ceecac2dad3050966821018e696
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-12 01:47:55 +0700

    test

---
+++ application/controllers/uploadphoto.php
@@ -0,0 +1,49 @@
+<?php
+class Uploadphoto extends CI_Controller {
+
+	public function uploadprofile(){
+		$ID = $this->session->all_userdata();
+		
+		$this->config =  array(
+			'file_name'    => "".$ID.".jpg", //name
+			'upload_path'     => "./files/",
+			'allowed_types'   => "gif|jpg|png|jpeg",
+			'overwrite'       => TRUE,
+			'max_size'        => "1000KB",
+			'max_height'      => "768",
+			'max_width'       => "1024"  
+		);
+		$this->load->library('upload', $this->config);
+		
+		if($this->upload->do_upload()){
+			echo "file upload success";
+		}
+		else{
+			echo "file upload failed";
+		}
+		
+	}
+	/*
+	public function uploaded(){
+
+		$this->config =  array(
+			'file_name'    => "eiei.jpg", //ส่วนเปลี่ยนชื่อรูป
+			'upload_path'     => "./files/",
+			'allowed_types'   => "gif|jpg|png|jpeg",
+			'overwrite'       => TRUE,
+			'max_size'        => "1000KB",
+			'max_height'      => "768",
+			'max_width'       => "1024"  
+		);
+		$this->load->library('upload', $this->config);
+		
+		if($this->upload->do_upload()){
+			echo "file upload success";
+		}
+		else{
+			echo "file upload failed";
+		}
+	}
+	*/
+}
+?>
--- application/views/editprofile.php
+++ application/views/editprofile.php
@@ -115,7 +115,7 @@
 	<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
 		<div class="modal-dialog modal-sm">
 			<div class="modal-content">
-				<form action="../Upload/uploaded" method="POST" enctype="multipart/form-data" >
+				<form action="../../uploadphoto/uploadprofile" method="POST" enctype="multipart/form-data" >
 					Select File To Upload:<br />
 					<input type="file" name="userfile"  />
 					<br /><br />
