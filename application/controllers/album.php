689b4c94c086c72546c4f4be1a92d9579aa84050
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-13 04:27:37 +0700

    test

--- application/controllers/album.php
+++ application/controllers/album.php
@@ -70,15 +70,10 @@ class Album extends CI_Controller {
 	}
 	
 	public function delete(){
-		if($_POST["checkdelete"] != NULL){
-			$checkdelete = $_POST["checkdelete"];
-			$ID = $this->session->all_userdata();
-			$this->db->delete('album', array('nameuser' => $ID['ID'],'namealbum'=>$checkdelete));
-			//$this->db->where('nameuser', $ID['ID'])->where('namealbum',$checkdelete);
-			//$this->db->delete('album', $checkdelete);
-			echo"<script language='javascript'>window.location.href = 'show/".$ID['ID']."';</script>";
-		}
-		else echo"<script language='javascript'>window.location.href = 'show/".$ID['ID']."';</script>";
+		$checkdelete = $_POST["checkdelete"];
+		$ID = $this->session->all_userdata();
+		$this->db->delete('album', array('nameuser' => $ID['ID'],'namealbum'=>$checkdelete));
+		echo"<script language='javascript'>window.location.href = 'show/".$ID['ID']."';</script>";
 	}
 	
 }
