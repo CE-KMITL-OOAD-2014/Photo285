8a62be1b1826ef44e785a65195523ad446c5be4f
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-13 03:07:58 +0700

    test

--- application/views/albumpage.php
+++ application/views/albumpage.php
@@ -220,16 +220,26 @@ img {
 															<h4 class='modal-title' id='mySmallModalLabel'>Delete Album :</h4>   <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
 														</div>
 														<form action='../create' method='POST' enctype='multipart/form-data' >";
-														for($i=0; $i <count($album);$i++) //วนลูปตามจำนวน album ที่มี เพื่อ สร้าง ให้ได้ตามจำนวนอัลบั้ม
-														{
-															echo"
-															<span class='button-checkbox'>
-																<button type='button' class='btn' data-color='primary'>".$Albumname."</button>
-																<input type='checkbox' class='hidden'/>
-															</span>
-															<hr>
-															";
-														}
+															 
+															$showal = $this->db->where('nameuser',$this->uri->segment(3))->get('album');
+															if($this->db->where('nameuser',$this->uri->segment(3))->count_all_results('album')==0)//ตรวจสอบว่า มีอัลบั้มไหม? ถ้าไม่มี ทำ if
+															{ 
+																echo" 
+																<center>
+																<h4>You don't have any album</h4>
+																</center>";
+															}
+															else // ตรวจสอบแล้ว มีอัลบั้ม ทำ else
+															foreach($showal->result_array() as $row) //วนลูปตามจำนวน album ที่มี เพื่อ สร้าง ให้ได้ตามจำนวนอัลบั้ม
+															{
+																echo"
+																<span class='button-checkbox'>
+																	<button type='button' class='btn' data-color='primary'>".$row['namealbum']."</button>
+																	<input type='checkbox' class='hidden'/>
+																</span>
+																<hr>
+																";
+															}
 															echo"<center>
 															<input type='submit' name='submit' value='Summit' class='btn btn-success' />   
 															</center>
