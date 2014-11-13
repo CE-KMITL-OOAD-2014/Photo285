b488aaddb003ea6590dfd70896ed08e9a18dd7ad
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-13 12:33:05 +0700

    test

--- application/views/albumpage.php
+++ application/views/albumpage.php
@@ -314,7 +314,7 @@ img {
 								</div>
 								<div class='panel-footer'>
 									<center>
-										<h4><a href='../../photo/show/".$this->uri->segment(3)."/".$row['namealbum']."' title='".$row['namealbum']." Album' style='text-decoration:none;'".$row['namealbum']."</a></h4>
+										<h4><a href='../../photo/show/".$this->uri->segment(3)."/".$row['namealbum']."' title='".$row['namealbum']." Album'".$row['namealbum']."</a></h4>
 									</center>
 								</div>
 							</div>
