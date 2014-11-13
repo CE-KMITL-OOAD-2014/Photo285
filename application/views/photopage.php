67bfde7e26117ec1092622bce5a6e5bbda20bfcc
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-13 21:15:58 +0700

    col photopage

--- application/views/photopage.php
+++ application/views/photopage.php
@@ -375,6 +375,7 @@ html, body { height: 100%;}
 							else {// ตรวจสอบแล้ว มีรูป ทำ else
 							foreach($showpic->result_array() as $row) //วน for เพื่อให้ รูปขึ้น
 							echo"
+							<div class='col-md-2 col-xs-6 col-sm-4'> 
 							<img style='margin-top:9px;' src='../../../../photo/".$row['ID'].".jpg' class='img-rounded'> <!--รูปในอัลบั้ม-->
 							&nbsp
 							";
