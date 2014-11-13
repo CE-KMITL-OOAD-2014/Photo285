72fa8e3a33e8d447fc77109a7b2e1490e105345c
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-13 12:58:02 +0700

    test

--- application/controllers/photo.php
+++ application/controllers/photo.php
@@ -3,10 +3,10 @@
 class Photo extends CI_Controller {
 
 	public function show(){
+		$this->load->view('navbar');
 		//echo $this->uri->segment(3); // nameuser
 		//echo $this->uri->segment(4); // namealbum
 		$showpic = $this->db->where('nameuser',$this->uri->segment(3))->where('namealbum',$this->uri->segment(4))->get('picture');
-		$this->load->view('navbar');
 		foreach($showpic->result_array() as $row){
 			echo "".$row['ID']." ";
 		}
