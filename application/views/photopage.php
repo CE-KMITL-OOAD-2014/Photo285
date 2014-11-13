62a5e7f01472517666ba2b5ba4a4144c391a88a1
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-13 21:17:21 +0700

    test

--- application/views/photopage.php
+++ application/views/photopage.php
@@ -376,8 +376,9 @@ html, body { height: 100%;}
 							foreach($showpic->result_array() as $row) //วน for เพื่อให้ รูปขึ้น
 							echo"
 							<div class='col-md-2 col-xs-6 col-sm-4'> 
-							<img style='margin-top:9px;' src='../../../../photo/".$row['ID'].".jpg' class='img-rounded'> <!--รูปในอัลบั้ม-->
-							&nbsp
+								<img style='margin-top:9px;' src='../../../../photo/".$row['ID'].".jpg' class='img-rounded'> <!--รูปในอัลบั้ม-->
+								&nbsp
+							</div>
 							";
 							}
 							?>
