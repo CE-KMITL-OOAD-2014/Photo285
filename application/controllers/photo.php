3bd91c6266385644059cd364a6d59fc1e54c5979
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-13 17:00:51 +0700

    test

--- application/controllers/photo.php
+++ application/controllers/photo.php
@@ -3,14 +3,22 @@
 class Photo extends CI_Controller {
 
 	public function show(){
-		$this->load->view('navbar');
 		//echo $this->uri->segment(3); // nameuser
 		//echo $this->uri->segment(4); // namealbum
-		$showpic = $this->db->where('nameuser',$this->uri->segment(3))->where('namealbum',$this->uri->segment(4))->get('picture');
-		foreach($showpic->result_array() as $row){
-			echo "".$row['ID']." ";
-		}
-		$this->load->view('photopage');
+		//$showpic = $this->db->where('nameuser',$this->uri->segment(3))->where('namealbum',$this->uri->segment(4))->get('picture');
+		//foreach($showpic->result_array() as $row){
+		//	echo "".$row['ID']." ";
+		//}
+		$id = $this->uri->segment(3); // ถ้ามี / อันที่ 3 เช่น (web.net/profile/show/admin) user คือ admin ก็เอามาเก็บในตัวแปล id
+		$albumname = $this->uri->segment(4);	
+		//load all
+		$this->load->model('member_model');
+		$name = $this->member_model->getName($id);
+		$email = $this->member_model->getEmail($id);
+		$resume = $this->member_model->getResume($id);
+		$data = array('name' => $name , 'id' => $id , 'email' => $email, 'resume' => $resume , 'albumname' => $albumname);
+		$this->load->view('navbar');
+		$this->load->view('photopage',$data);
 		
 	}	
 }
--- application/views/photopage.php
+++ application/views/photopage.php
@@ -305,13 +305,13 @@ html, body { height: 100%;}
   </script>
 	
 	</head>
-	<body background="img/white.jpg">
+	<body background="../../../../img/white.jpg">
 	   <div class="container">
 			<div class="col-md-3 col-xs-12 col-sm-12">
 				<div class="col-md-12 col-xs-12 col-sm-12">
 					<center>
 						<a href="#aboutModal" data-toggle="modal" data-target="#myModal"><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R" name="aboutme" width="140" height="140" class="img-circle"></a>
-						<h3>Joe Sixpack</h3>
+						<h3><? echo "".$name; ?></h3>
 					</center>
 				</div>
 				<div class="row">
@@ -345,31 +345,24 @@ html, body { height: 100%;}
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-							<h4 class="modal-title" id="myModalLabel">More About Joe</h4>   <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
+							<h4 class="modal-title" id="myModalLabel"><? echo "".$id; ?></h4>   <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
 						</div>
 						<div class="modal-body">
 							<center>
 								<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
-									<h3 class="media-heading">Joe Sixpack </h3> <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
+									<h3 class="media-heading"><? echo "".$name; ?></h3> <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
 									<span><strong>Email: </strong></span>  <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
-									<span>joe.sixpack@hotmail.com</span>  <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
+									<span><? echo "".$email; ?></span>  <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
 							</center>
 							<hr>
 							<center>
-								<p class="text-left"><strong>Annouce: </strong><br>
-									I am looking for someone with honesty and integrity. 
-									Please be who you say you are–please no game players! 
-									Please show a recent photo (get out of denial, yes, you 
-									and I both have aged–hopefully, gracefully, lol!) 
-									We all wish we looked and felt the way we did 10 years ago! :)
-									A person with a good sense of humor… A smile goes a long way–make someone happy today! 
-									My profile says I am 50… Sorry, I wasn’t able to change it… I am ….. 56!</p>   <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
+								<p class="text-left"><strong>ประวัติส่วนตัว: </strong><br><? echo "".$resume; ?></p>   <!--ต้องเปลี่ยนเป็นชื่อคัวแปร-->
 								<br>
 							</center>
 						</div>
 						<div class="modal-footer">
 							<center>
-								<button type="button" class="btn btn-default" data-dismiss="modal">I've heard enough about Joe</button>
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 							</center>
 						</div>
 					</div>
@@ -379,7 +372,7 @@ html, body { height: 100%;}
 			<div class="col-md-9 col-xs-12 col-sm-12"> <!---ส่วนแสดงรูปในอัลบั้ม-->
 				<div class="container">
 					
-						<h1 class="muted">Album name</h1>
+						<h1 class="muted"><? echo "".$albumname; ?></h1>
 						<hr>
 							<!--?php
 								 for($i = 0 ;$i<$x ;$i++){
