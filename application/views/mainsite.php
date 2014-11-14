06cd6ef564a2a50f9e4d328dd827e440b5e110cb
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-14 20:34:53 +0700

    test

--- application/views/mainsite.php
+++ application/views/mainsite.php
@@ -473,12 +473,7 @@ $('#mytextbox').on("paste",function(e)
 						</div>
 						<div class="col-md-12">
 							<div class="col-md-6">
-								<div class="row"><div<em><label><font color="white">Don't have an account?</font></label></em></div>
 								<button class="btn btn-lg btn-success btn-block" type="submit">Log in</button></br>
-							</div>
-							<div class="col-md-6">
-								<em><label>Don't have an account?</label></em>
-								<button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up Here</button></br>
 							</div>
 						</div>
 					</form>
