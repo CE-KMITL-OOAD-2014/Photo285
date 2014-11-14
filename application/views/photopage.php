47e6307255dc2ba89511146556dd1c9c4dcf080b
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-14 17:27:35 +0700

    test

--- application/views/photopage.php
+++ application/views/photopage.php
@@ -9,161 +9,12 @@
     <!-- Bootstrap -->
     <link href="../../../../css/bootstrap.min.css" rel="stylesheet">
 	<style>
-	 /* CSS used here will be applied after bootstrap.css */
-
-html, body { height: 100%;}
-
-/* So that the modal is displayed in the preview.. You can probably remove this and the above rule */
-.img-modal {
-  display: block;
-}
-
-.img-modal .modal-dialog {
-    /* An arbitrary minimum height. Feel free to modify this one as well */
-    min-height: 350px;
-    height: 80%;
-}
-
-.img-modal .modal-content, .img-modal .modal-body, .img-modal .row, .img-modal .modal-image {
-    height: 100%;
-}
-
-.modal-content {
-    border-radius: 0;
-}
-
-.modal-body {
-    padding-top: 0;
-    padding-bottom: 0;
-}
-
-.modal-image {
-    background: #000;
-    padding :0;
-}
-
-.modal-image img {
-    margin: 0 auto;
-    max-height: 100%;
-    max-width: 100%;
-    
-    position: relative;
-    top: 50%;
-    -webkit-transform: translateY(-50%);
-    -ms-transform: translateY(-50%);
-    transform: translateY(-50%);
-}
-
-.img-modal .img-modal-btn {
-    display: block;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    background: black;
-    opacity: 0;
-    font-size: 1.5em;
-    width: 45px;
-    color: #fff;
-    transition: opacity .2s ease-in;
-}
-
-.img-modal .modal-image:hover .img-modal-btn {
-    opacity: 0.4;  
-}
-
-.img-modal .modal-image:hover .img-modal-btn:hover {
-   opacity: 0.75;   
-}
-
-.img-modal .img-modal-btn.right {
-    right: 0;
-}
-
-.img-modal .img-modal-btn i {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    right: 0;
-    text-align: center;
-    margin-top: -.75em;
-}
-
-.img-modal .modal-meta {
-    position: relative;   
-    height: 100%;
-}
-
-.img-modal .modal-meta-top {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 45px;
-    padding: 5px 10px;
-    overflow-y: auto;
-}
-
-.img-modal .modal-meta-top .img-poster img {
-    height: 70px;
-    width: 70px;
-    float: left;
-    margin-right: 15px;
-}
-
-.img-modal .modal-meta-top .img-poster strong {
-    display: block; 
-    padding-top: 15px;
-}
-
-.img-modal .modal-meta-top .img-poster span {
-    display: block;   
-    color: #aaa;
-    font-size:.9em;
-}
-
-.img-modal .modal-meta-bottom {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    padding: 5px;
-    border-top: solid 1px #ccc;
-}
-
-.img-modal .img-comment-list {
-    list-style: none;  
-    padding: 0;
-}
-.img-modal .img-comment-list li {
-    margin:0;
-    margin-top:10px;
-}
-
-.img-modal .img-comment-list li > div {
-    display:table-cell;
-}
-
-.img-modal .img-comment-list img {
-    border-radius:50%;
-    width: 42px;
-    height: 42px;
-    margin-right: 10px;
-    margin-top: 20px;
-}
-
-.img-modal .img-comment-list p {
-    margin: 0;
-}
-
-.img-modal .img-comment-list span {
-    font-size: .8em; 
-    color: #aaa;
-}	
+	
 .chkhidden{
 		display:none;
 		position: absolute;
 	}
-<!--ส่วน comment_like-->
+
 	</style>
 	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -273,7 +124,7 @@ html, body { height: 100%;}
         
         return false;
     })
-	$('#photoModal').modal('toggle').click(); <!--ส่วนนี้ทำให้คลิก แล้ว modal เด้ง--->
+	
 });
   </script>
   <script> <!--ส่วนนี้ นับ LIKE--ตรงนี้ต้องแก้ให้มัน กดซ้ำ แล้ว UNLIKE->
@@ -292,9 +143,7 @@ html, body { height: 100%;}
         $(this).ekkoLightbox();
     });                                        
 });
-	function photoToggle(){
-	$('#photoModal').modal('toggle');
-	}
+	
   </script>
 	
 	</head>
@@ -379,7 +228,7 @@ html, body { height: 100%;}
 							foreach($showpic->result_array() as $row) //วน for เพื่อให้ รูปขึ้น
 							echo"
 							<div class='col-md-3 col-xs-6 col-sm-4'> 
-								<img style='margin-top:9px;' src='../../../../photo/".$row['ID'].".jpg' class='img-rounded' width='150' height='150' onclick='photoToggle();'> <!--รูปในอัลบั้ม-->
+								<a href='../../../photo/show/".$this->uri->segment(3)."/".$this->uri->segment(4)."/".$row['ID']."'><img style='margin-top:9px;' src='../../../../photo/".$row['ID'].".jpg' class='img-rounded' width='150' height='150'></a> <!--รูปในอัลบั้ม-->
 								&nbsp
 							</div>
 							";
@@ -392,103 +241,6 @@ html, body { height: 100%;}
 		</div>
 
 
-		<!-- Modal -->
-<div class="modal img-modal" id="photoModal">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-body">
-			
-            <div class="col-md-8 col-sm-8 col-xs-8 modal-image">
-                 <img class="img-responsive " src="http://www.pimart.eu/wp-content/uploads/2014/04/martin-schoeller-george-clooney-portrait-up-close-and-personal.jpeg">        
-				<a href="" class="img-modal-btn right"><i id="like1" class="glyphicon glyphicon-thumbs-up"></i> <div id="like1-bs3"></div></a>
-          	</div>
-        	<div class="col-md-4 col-sm-4 col-xs-4 modal-meta">
-              <div class="modal-meta-top">
-                  <button type="button" class="close"  data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-                  <div class="img-poster clearfix">
-						<a href=""><img class="img-circle" src="http://www.localcrimenews.com/wp-content/uploads/2013/07/default-user-icon-profile.png"/><!--รูปของเจ้าของภาพ--></a>
-						<strong><a href="">Jone Doe<!--ชื่อคนcomment--></a></strong>
-						<button type="button" class="btn btn-default btn-md">
-							<span class="glyphicon glyphicon-trash"  aria-hidden="true" ></span>
-						</button>
-						<button  class="btn btn-default btn-md" data-toggle="modal" data-target=".bs-example-modal-lg4" ><span class="glyphicon glyphicon-globe" > เลือกรูปหน้าหลัก</span></button>
-					</div>
-					<ul class="img-comment-list">
-					<?if($comment==NULL)//ตรวจสอบว่า มีcomment ไหม? ถ้าไม่มี ทำ if
-					{ echo"<center>
-								<h4>You don't have any comment just test ,</h4>
-							</center>";//เข้า if (ไม่มี comment ก็ไม่ต้องทำอะไร
-					}
-					  else 
-					  for($i=0; $i <count($comment) ; $i++) //วน comment เพื่อให้รูปขึ้น
-						{echo"
-						<li>
-							<div class='comment-img'>
-							  <img src='http://lorempixel.com/50/50/people/6'>
-							</div>
-							<div class='comment-text'>
-								<strong><a href=''>".$UserComment."</a></strong>
-								<p>".$commentMessage."</p> 
-							</div>
-						</li>";
-						}
-					?>
-					</ul>
-              </div>
-              <div class="modal-meta-bottom">
-                  <input type="text" class="form-control" placeholder="Leave a commment.."/>
-              </div>
-          	</div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- Small modal --> <!--ส่วนที่ตัวเด้ง ตั้งรูปให้อยู่หน้าหลัก---> 
-											<div class='modal fade bs-example-modal-lg4' tabindex='-1' role='dialog' aria-labelledby='mySmallModalLabel' aria-hidden='true'>
-												<div class='modal-dialog modal-sm'>
-													<div class='modal-content'>
-														<div class='modal-header'>
-															<button type='button' class='close' data-dismiss='modal' aria-hidden='true'></button>
-															<h4 class='modal-title' id='mySmallModalLabel'>เลือกรูปให้อยู่ในหน้าหลัก :</h4>  
-														</div>
-														<form action='....' method='POST' enctype='multipart/form-data' >
-															
-																<div class='radio'>
-																  <label>
-																	<input type='radio' name='checkdelete' id='optionsRadios1' value=''>
-																	รูปหน้าหลักที่ 1
-																  </label>
-																</div>
-																
-																<div class='radio'>
-																  <label>
-																	<input type='radio' name='checkdelete' id='optionsRadios1' value=''>
-																	รูปหน้าหลักที่ 2
-																  </label>
-																</div>
-																
-																<div class='radio'>
-																  <label>
-																	<input type='radio' name='checkdelete' id='optionsRadios1' value=''>
-																	รูปหน้าหลักที่ 3
-																  </label>
-																</div>
-																
-																<div class='radio'>
-																  <label>
-																	<input type='radio' name='checkdelete' id='optionsRadios1' value=''>
-																	รูปหน้าหลักที่ 4
-																  </label>
-																</div>
-															
-															<hr>
-															<center>
-															<input type='submit' name='submit' value='Summit' class='btn btn-success' />   
-															</center>
-														</form>
-													</div>
-												</div>
-											</div><!---กดตกลงเพื่อเลือกรูปไปหน้าหลัก-->
+		
 	</body>
 </html>
