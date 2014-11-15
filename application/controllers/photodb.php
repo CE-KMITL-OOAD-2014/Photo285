e9faf3fd301d3def383e06a1b8fb182968465274
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-16 00:13:35 +0700

    test

--- application/controllers/photodb.php
+++ application/controllers/photodb.php
@@ -5,7 +5,7 @@ class Photodb extends CI_Controller {
 		$ID = $this->session->all_userdata();
 		
 		$this->config =  array(
-			'file_name'    => "".$ID['ID'], //name
+			'file_name'    => "".$ID['ID'].".jpg", //name
 			'upload_path'     => "./photo/profile",
 			'allowed_types'   => "jpg|png|jpeg|gif",
 			'overwrite'       => TRUE,
