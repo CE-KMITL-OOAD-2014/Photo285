c2b3039a52bc8c3cb7b0f9d0546b0adc7ee4d0a4
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-16 04:26:25 +0700

    test

--- application/models/like_model.php
+++ application/models/like_model.php
@@ -1,16 +1,25 @@
 <?php
 	class Like_model extends CI_Model{
 		function addlike($data){
-			$this->db->select_max('ID');
-			$sID = $this->db->get('likedb');
-			$sID = $sID->result_array();
-			foreach($sID as $row) $sID = $row['ID']+1;
-			$likeup = 1;
-			$datadb = array('ID'=>$sID,'likeup'=>$likeup,'idphoto'=>$data['idphoto'],'nameuser'=>$data['iduser']);
-			$this->db->insert('likedb',$datadb);
-			echo "<script language='javascript'>
-					window.location.href = '../../../../onephoto/show/".$data['idphoto']."/';
-				</script>";
+			$check = $this->db->where('idphoto',$data['idphoto'])->where('nameuser',$data['iduser'])->count_all_results('likedb');
+			if($check==1){
+				echo "<script language='javascript'>
+						window.location.href = '../../../../onephoto/show/".$data['idphoto']."/';
+					</script>";
+			}
+			
+			else{
+				$this->db->select_max('ID');
+				$sID = $this->db->get('likedb');
+				$sID = $sID->result_array();
+				foreach($sID as $row) $sID = $row['ID']+1;
+				$likeup = 1;
+				$datadb = array('ID'=>$sID,'likeup'=>$likeup,'idphoto'=>$data['idphoto'],'nameuser'=>$data['iduser']);
+				$this->db->insert('likedb',$datadb);
+				echo "<script language='javascript'>
+						window.location.href = '../../../../onephoto/show/".$data['idphoto']."/';
+					</script>";
+			}
 		}	
 	}
 ?>                                                               
