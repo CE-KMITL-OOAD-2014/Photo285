afe5dc84112fc9216068a1982edeabd5684d2cad
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-16 03:19:58 +0700

    test

--- application/views/onephoto.php
+++ application/views/onephoto.php
@@ -303,8 +303,7 @@ html, body { height: 100%;}
 					<div class="modal-body">
 						<div class="col-md-8 col-sm-8 col-xs-8 modal-image">
 							<img class="img-responsive " src="../../../../photo/<? echo "".$this->uri->segment(3).".jpg"; ?>">        
-							<a href="../../" class="btn img-modal-btn right"><i class="glyphicon glyphicon-thumbs-up"></i> <div id="like1-bs3"></div></a>
-							<a href="../../" class="btn btn-warning"><div class="link">cancel</div></a>
+							<a href="www.google.co.th"  class="img-modal-btn right"><i id="like1" class="glyphicon glyphicon-thumbs-up"></i> <div id="like1-bs3"></div></a>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 modal-meta">
 							<div class="modal-meta-top">
