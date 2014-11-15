25e62478aac75e01f02d70c97b3ed37bfc9e751d
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-16 04:45:13 +0700

    add footer

---
+++ application/views/footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Photo285 : Upload Your Picture</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <style>
+  #footerBar {
+    height: 70px;
+    background: #F5FFFA	
+}
+
+  </style>
+  <body >
+	<br>
+	<br>
+    <footer id="footerBar">
+		<div class="col-md-10 col-sm-10 col-xs-10"></div>
+		<div class="col-md-2 col-sm-2 col-xs-2">
+			<em><strong><center><label>special thanks<label></center></strong></em>
+			<div class="col-md-2 col-sm-2 col-xs-2">
+				<label> credit: </label>
+			</div>
+			<div class="col-md-10 col-sm-10 col-xs-10">
+				<a href="http://getbootstrap.com/">http://getbootstrap.com/ </a>
+				<a href="http://bootsnipp.com/">http://bootsnipp.com/</a>	
+			</div>
+		</div>
+	</footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
