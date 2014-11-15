0bbd85557e48ceb309bb5ea7ccd5acb6ce1c50e7
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-16 04:18:11 +0700

    test

--- application/models/like_model.php
+++ application/models/like_model.php
@@ -2,11 +2,11 @@
 	class Like_model extends CI_Model{
 		function addlike($data){
 			$this->db->select_max('ID');
-			$sID = $this->db->get('like');
+			$sID = $this->db->get('likedb');
 			$sID = $sID->result_array();
 			foreach($sID as $row) $sID = $row['ID']+1;
-			$like = 1;
-			$datadb = array('ID'=>$sID,'like'=>$like,'idphoto'=>$data['idphoto'],'nameuser'=>$data['iduser']);
+			$likeup = 1;
+			$datadb = array('ID'=>$sID,'likeup'=>$likeup,'idphoto'=>$data['idphoto'],'nameuser'=>$data['iduser']);
 			$this->db->insert('like',$datadb);
 			echo "<script language='javascript'>
 					window.location.href = '../../../../onephoto/show/".$data['idphoto']."/';
