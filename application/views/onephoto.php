f9724fae14a11db95aa45c0e138b2d6c81f169cf
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-15 20:57:38 +0700

    test

--- application/views/onephoto.php
+++ application/views/onephoto.php
@@ -316,7 +316,7 @@ html, body { height: 100%;}
 										if($ID['ID']==$idprofile){
 											echo"<button type='button' class='btn btn-default btn-md'>
 													<span class='glyphicon glyphicon-trash'  aria-hidden='true' data-toggle='modal' data-target='.bs-example-modal-lg5'></span>
-												</button>"
+												</button>";
 											echo "<button  class='btn btn-default btn-md' data-toggle='modal' data-target='.bs-example-modal-lg4' ><span class='glyphicon glyphicon-globe' > เลือกรูปหน้าหลัก</span></button>";
 										}
 									}
