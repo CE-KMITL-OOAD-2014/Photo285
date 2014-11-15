a89d7351942c53e76e8c658bfa2ff4f8f558651f
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-15 19:15:29 +0700

    test

--- application/views/editprofile.php
+++ application/views/editprofile.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+	<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,98 +15,98 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
-   <style> <!---css ที่ตั้งค่า -->
-  .link <!--ตั้งค่าให้ปุ่ม href เปลี่ยนเอักษรเป็นสีขาว-->
-{
-   color:white;
-   text-decoration: none; 
-   background-color: none;
-}
-  </style> <!--เป็นการ import template มาจาก bootstrap.com--->
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="../js/bootstrap.min.js"></script>
+	</head>
+	<style> <!---css ที่ตั้งค่า -->
+	  .link <!--ตั้งค่าให้ปุ่ม href เปลี่ยนเอักษรเป็นสีขาว-->
+	{
+	   color:white;
+	   text-decoration: none; 
+	   background-color: none;
+	}
+	</style> <!--เป็นการ import template มาจาก bootstrap.com--->
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="../js/bootstrap.min.js"></script>
   
-  <body background="../../img/white.jpg"> <!---body พร้อม background-->
-    <form role="form" action="../member/editProfileSend" method="post"> <!---ส่วนกรอกฟอร์ม ที่ไว้กรอก---> 
-		<div class ="col-md-2"></div>	
-		<div class ="col-md-8">	
-			<h2> Edit Profile</h2><!--คำพูดหัวข้อ-->
-			<hr>
-			<div class ="col-md-4">	
-				<center>
-					<a href="#aboutModal" ><img src="../../photo/profile/<?$ID = $this->session->all_userdata(); echo "".$ID['ID'].".jpg" ?>" name="aboutme" width="140" height="140" class="img-circle"></a></br><br/>
-					<a class="btn btn-primary " data-toggle="modal" data-target=".bs-example-modal-sm">Upload photo</a>
-				</center>
-			</div>
+	<body background="../../img/white.jpg"> <!---body พร้อม background-->
+		<form role="form" action="../member/editProfileSend" method="post"> <!---ส่วนกรอกฟอร์ม ที่ไว้กรอก---> 
+			<div class ="col-md-2"></div>	
 			<div class ="col-md-8">	
-				<div class="col-md-12">  <!--ส่วนกรอก username----A-->
-					<div class="form-group">
-						<div class="col-md-12">
-							<label for="exampleInputUsername1">Username</label>
+				<h2> Edit Profile</h2><!--คำพูดหัวข้อ-->
+				<hr>
+				<div class ="col-md-4">	
+					<center>
+						<a href="#aboutModal" ><img src="../../photo/profile/<?$ID = $this->session->all_userdata(); echo "".$ID['ID'].".jpg" ?>" name="aboutme" width="140" height="140" class="img-circle"></a></br><br/>
+						<a class="btn btn-primary " data-toggle="modal" data-target=".bs-example-modal-sm">Upload photo</a>
+					</center>
+				</div>
+				<div class ="col-md-8">	
+					<div class="col-md-12">  <!--ส่วนกรอก username----A-->
+						<div class="form-group">
+							<div class="col-md-12">
+								<label for="exampleInputUsername1">Username</label>
+							</div>
+							<div class="col-md-12">
+								<input type="text" name="username" class="form-control " width = "50%" id="exampleInputUsername1" value="<? echo "".$id; ?>" readonly="readonly"required autofocus>
+							</div>
 						</div>
-						<div class="col-md-12">
-							<input type="text" name="username" class="form-control " width = "50%" id="exampleInputUsername1" value="<? echo "".$id; ?>" readonly="readonly"required autofocus>
+					</div> <!--ส่วนกรอก username------>
+							
+					<div class="col-md-12">  <!--ส่วนกรอก EMAIL----A-->
+						<div class="form-group">
+							<div class="col-md-12">
+								<label for="exampleInputEmail1">Email address</label>
+							</div>
+							<div class="col-md-12">
+								<input type="email" name="email" class="form-control" id="exampleInputEmail1" value="<? echo "".$email; ?>" readonly="readonly" required autofocus>
+							</div>
 						</div>
-					</div>
-				</div> <!--ส่วนกรอก username------>
-						
-				<div class="col-md-12">  <!--ส่วนกรอก EMAIL----A-->
-					<div class="form-group">
-						<div class="col-md-12">
-							<label for="exampleInputEmail1">Email address</label>
+					</div>  <!--ส่วนกรอก EMAIL------>
+							
+					<div class="col-md-12"> <!--ส่วนกรอก NAME----A-->
+						<div class="form-group">
+							<div class="col-md-12">
+								<label for="exampleInputName1">Name</label>
+							</div>
+							<div class="col-md-12">
+								<input type="text" name="name" class="form-control" id="exampleInputName1"  pattern=".{6,60}" required title="6 to 60 characters" value="<? echo "".$name; ?>" required autofocus>
+							</div>
 						</div>
-						<div class="col-md-12">
-							<input type="email" name="email" class="form-control" id="exampleInputEmail1" value="<? echo "".$email; ?>" readonly="readonly" required autofocus>
+					</div>  <!--ส่วนกรอก NAME------>					
+							
+					<div class="col-md-12">  <!--ส่วนกรอก DESCRIPTION----->
+						<div class="form-group">
+							<div class="col-md-12">
+								<label for="exampleInputEmail1">ประวัติส่วนตัว</label>
+							</div>
+							<div class="col-md-12">
+								<input type="message" name="resume" class="form-control" id="des" value="<? echo "".$resume; ?>">
+							</div>
 						</div>
-					</div>
-				</div>  <!--ส่วนกรอก EMAIL------>
-						
-				<div class="col-md-12"> <!--ส่วนกรอก NAME----A-->
-					<div class="form-group">
-						<div class="col-md-12">
-							<label for="exampleInputName1">Name</label>
+					</div>  <!--ส่วนกรอก DESCRIPTION----->
+							
+					<div class="col-md-12">  <!--ส่วนกรอก NOTICE--->
+						<div class="form-group">
+							<div class="col-md-12">
+								<label for="exampleInputEmail1">ข่าวสาร</label>
+							</div>
+							<div class="col-md-12">
+								<input type="text" name="detail" maxlength="64" class="form-control" id="test" value="<? echo "".$detail; ?>">
+							</div>
 						</div>
+					</div>  <!--ส่วนกรอก NOTICE----->
+				
+					<div class="col-md-12"> <!------ส่วนกด SUMMIT--->
 						<div class="col-md-12">
-							<input type="text" name="name" class="form-control" id="exampleInputName1"  pattern=".{6,60}" required title="6 to 60 characters" value="<? echo "".$name; ?>" required autofocus>
+							</br>
+							<button type="submit" value="OK" id ="regisbutton" class="btn btn-success">Submit</button>
+							<a href="../../profile/show/<? echo "".$id; ?>" id ="cancel" class="btn btn-warning"><div class="link">cancel</div></a>
 						</div>
-					</div>
-				</div>  <!--ส่วนกรอก NAME------>					
-						
-				<div class="col-md-12">  <!--ส่วนกรอก DESCRIPTION----->
-					<div class="form-group">
-						<div class="col-md-12">
-							<label for="exampleInputEmail1">ประวัติส่วนตัว</label>
-						</div>
-						<div class="col-md-12">
-							<input type="message" name="resume" class="form-control" id="des" value="<? echo "".$resume; ?>">
-						</div>
-					</div>
-				</div>  <!--ส่วนกรอก DESCRIPTION----->
-						
-				<div class="col-md-12">  <!--ส่วนกรอก NOTICE--->
-					<div class="form-group">
-						<div class="col-md-12">
-							<label for="exampleInputEmail1">ข่าวสาร</label>
-						</div>
-						<div class="col-md-12">
-							<input type="text" name="detail" maxlength="64" class="form-control" id="test" value="<? echo "".$detail; ?>">
-						</div>
-					</div>
-				</div>  <!--ส่วนกรอก NOTICE----->
-			
-				<div class="col-md-12"> <!------ส่วนกด SUMMIT--->
-					<div class="col-md-12">
-						</br>
-						<button type="submit" value="OK" id ="regisbutton" class="btn btn-success">Submit</button>
-						<a href="../../profile/show/<? echo "".$id; ?>" id ="cancel" class="btn btn-warning"><div class="link">cancel</div></a>
-					</div>
-				</div> <!------ส่วนกด SUMMIT--->
-			</div>	
-		</div>
-	</form>
+					</div> <!------ส่วนกด SUMMIT--->
+				</div>	
+			</div>
+		</form>
 					
     
 	<!-- Small modal ที่เด้งมาให้เลือกอัพโหลดไฟล์ที่จะเป็น Profile picture-->
