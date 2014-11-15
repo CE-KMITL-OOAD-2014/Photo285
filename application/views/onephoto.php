36697505a1b1d884f46fb959873a5f3d3afbeca3
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-16 01:12:53 +0700

    test

--- application/views/onephoto.php
+++ application/views/onephoto.php
@@ -314,8 +314,8 @@ html, body { height: 100%;}
 									<?if($this->session->userdata('ID')){
 										$ID = $this->session->all_userdata();
 										if($ID['ID']==$idprofile){
-											echo"<button type='button' class='btn btn-default btn-md'>
-													<span class='glyphicon glyphicon-trash'  aria-hidden='true' data-toggle='modal' data-target='.bs-example-modal-lg5'>.</span>
+											echo"<button class='btn btn-default btn-md' data-toggle='modal' data-target='.bs-example-modal-lg5'>
+													<span class='glyphicon glyphicon-trash'  aria-hidden='true'></span>
 												</button>";
 											echo "<button  class='btn btn-default btn-md' data-toggle='modal' data-target='.bs-example-modal-lg4' ><span class='glyphicon glyphicon-globe' > เลือกรูปหน้าหลัก</span></button>";
 										}
