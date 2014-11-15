89dc8cc60036f14dfa5926866fdbcc7b5a0899bc
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-15 23:09:33 +0700

    test

--- application/views/albumpage.php
+++ application/views/albumpage.php
@@ -182,37 +182,18 @@
 		}
 		</script>
 		<script>
-			$(document).ready(function(){
-			$("#mytextbox").on("keypress", function(event) {
-
-			// Disallow anything not matching the regex pattern (A to Z uppercase, a to z lowercase and white space)
-			// For more on JavaScript Regular Expressions, look here: https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Regular_Expressions
-			var englishAlphabetAndWhiteSpace = /[A-Za-z ]/g;
-		   
-			// Retrieving the key from the char code passed in event.which
-			// For more info on even.which, look here: http://stackoverflow.com/q/3050984/114029
-			var key = String.fromCharCode(event.which);
-			
-			//alert(event.keyCode);
-			
-			// For the keyCodes, look here: http://stackoverflow.com/a/3781360/114029
-			// keyCode == 8  is backspace
-			// keyCode == 37 is left arrow
-			// keyCode == 39 is right arrow
-			// englishAlphabetAndWhiteSpace.test(key) does the matching, that is, test the key just typed against the regex pattern
-			 if (englishAlphabetAndWhiteSpace.test(key)) {
-				return true;
-			}
-
-			// If we got this far, just return false because a disallowed key was typed.
-			return false;
-		});
-
-		$('#mytextbox').on("paste",function(e)
-		{
-			e.preventDefault();
-		});
-		});
+			$("#mytextbox").keypress(function(event){
+    var ew = event.which;
+    if(ew == 32)
+        return true;
+    if(48 <= ew && ew <= 57)
+        return true;
+    if(65 <= ew && ew <= 90)
+        return true;
+    if(97 <= ew && ew <= 122)
+        return true;
+    return false;
+});
 		</script>
 	</head>
 	
