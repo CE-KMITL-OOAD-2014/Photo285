dad99bfd80e2790ad90de3f5adb582568046ca26
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-11-16 06:22:01 +0700

    commit test

--- application/views/albumpage.php
+++ application/views/albumpage.php
@@ -133,7 +133,7 @@
 														</div>
 														<form action='../create' method='POST' enctype='multipart/form-data' >
 														<em><label for='exampleInputName1'>Type Album Name</label></em>
-														<input  name='namealbum' id='mytextbox' class='' autocomplete='off' placeholder='aAllow Only [A-Za-z0-9]' tabindex='1' required='' autofocus='' style='background-color: rgb(247, 129, 129);'><br>
+														<input  name='namealbum' id='mytextbox' pattern='[A-Za-z\\s]*' class='form-control input-lg' autocomplete='off' placeholder='Allow Only [A-Za-z0-9]' tabindex='1' required='' autofocus='' style='background-color: rgb(247, 129, 129);'><br>
 															<center>
 															<input type='submit' name='submit' value='Summit'id='namalbumsummit' class='btn btn-success' />   
 															</center>
@@ -263,35 +263,7 @@
 		<script src="../../../js/font-awesome.min.css"></script>
 		<script src="../../../js/ekko-lightbox.js"></script> <!--lightbox--->
 		<script>
-			$("#mytextbox").on("keypress", function(event) {
-
-    // Disallow anything not matching the regex pattern (A to Z uppercase, a to z lowercase and white space)
-    // For more on JavaScript Regular Expressions, look here: https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Regular_Expressions
-    var englishAlphabetAndWhiteSpace = /[A-Za-z ]/g;
-   
-    // Retrieving the key from the char code passed in event.which
-    // For more info on even.which, look here: http://stackoverflow.com/q/3050984/114029
-    var key = String.fromCharCode(event.which);
-    
-    //alert(event.keyCode);
-    
-    // For the keyCodes, look here: http://stackoverflow.com/a/3781360/114029
-    // keyCode == 8  is backspace
-    // keyCode == 37 is left arrow
-    // keyCode == 39 is right arrow
-    // englishAlphabetAndWhiteSpace.test(key) does the matching, that is, test the key just typed against the regex pattern
-    if (event.keyCode == 8 || event.keyCode == 37 || event.keyCode == 39 || englishAlphabetAndWhiteSpace.test(key)) {
-        return true;
-    }
-
-    // If we got this far, just return false because a disallowed key was typed.
-    return false;
-});
-
-$('#mytextbox').on("paste",function(e)
-{
-    e.preventDefault();
-});
+			
 		</script>
 		<script> <!--นำมาจาก bootsnipp.com--->
 			$(function () {
