2f3bd38fe70432e39342acbe7e69bc590c1bc63c
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-15 21:29:32 +0700

    test

--- application/models/comment_model.php
+++ application/models/comment_model.php
@@ -1,15 +1,14 @@
 <?php
 	class Comment_model extends CI_Model{
 		function addcomment($data){
-			echo "".$data['comment'];
-			echo " ".$data['idphoto'];
-			echo " ".$data['iduser'];
 			//ทำเลข ID เพิ่มไปเรื่อยๆของ comment
 			$this->db->select_max('ID');
 			$sID = $this->db->get('comment');
 			$sID = $sID->result_array();
 			foreach($sID as $row) $sID = $row['ID']+1;
-			echo " ".$sID;
+			
+			$datadb = array('ID'=>$sID,'comment'=>$data['comment'],'idphoto'=>$data['idphoto'],'nameuser'=>$data['iduser']);
+			$this->db->insert('comment',$data);
 		}	
 	}
 ?>                                                               
