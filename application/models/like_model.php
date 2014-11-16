819d4457cee1a5aad2ed800d83f362b2b3bea823
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-16 14:46:23 +0700

    test

--- application/models/like_model.php
+++ application/models/like_model.php
@@ -3,7 +3,7 @@
 		function addlike($data){ //จัดเก็บ like ลง database
 			$check = $this->db->where('idphoto',$data['idphoto'])->where('nameuser',$data['iduser'])->count_all_results('likedb'); // จัดเก็บค่าที่เช็คว่า user เคยทำการกด like รูปภาพนี้แล้วหรือยัง
 			if($check==1){ // ถ้าเคยกดแล้วให้กลับไปแสดงผลหน้าเดิมไม่มีอะไรเปลี่ยนแปลง
-				$this->db->delete('likedb', array('nameuser' => $data['iduser'],'idphoto'=>$data['idphto'])); // ส่งค่าเข้าไปจัดเก็บใน database
+				$this->db->delete('likedb', array('nameuser' => $data['iduser'],'idphoto'=>$data['idphoto'])); // ส่งค่าเข้าไปจัดเก็บใน database
 				echo "<script language='javascript'>
 						window.location.href = '../../../../onephoto/show/".$data['idphoto']."/';
 					</script>";
