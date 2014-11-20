4a74c8f186746921e5d3b9de696a1acb71401d00
theeramed jitsukon <gamegolfg@hotmail.com>
2014-11-20 15:52:06 +0700

    test

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -4,6 +4,12 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
+		//redirect ไปหน้า profile กรณี login แล้ว
+		if($this->session->userdata('ID')){ // แสดงชื่อโปรไฟล์ตัวเอง
+			$ID = $this->session->all_userdata();
+			 header('Location: '.'../../../../profile/show/'.$ID["ID"]);
+		}
+		
 		$this->load->view('mainsite'); //เรียกใช้ mainsite เป็นหน้าเว็บหลัก
 		$this->load->view('footer');
 	}
